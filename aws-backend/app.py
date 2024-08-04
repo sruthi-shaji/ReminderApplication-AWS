5705fe4e05b87a990fe8d4fbca74954d017a722b
sruthi_shaji <sruthii.mec@gmail.com>
2024-08-04 00:55:55 -0300

    feat: added allow all host

--- aws-backend/app.py
+++ aws-backend/app.py
@@ -257,5 +257,5 @@ def fetch_reminder():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8000) 
+    app.run(host='0.0.0.0', port=8000) 
    
